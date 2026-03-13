f9e4327738f9cfd8d7e249a7a7d2d56dd67f1ec4
Aditya Singh <singh.aditya.44618@gmail.com>
2026-03-13 18:11:22 +0530

    url changes

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-import "dotenv/config"
+import "dotenv/config";
 
-import app from "./app.js"
-import connectDB from "./src/config/db.js"
+import app from "./app.js";
+import connectDB from "./src/config/db.js";
 import { updateMissingDays } from "./src/services/nseService.js";
 import startNSESyncJob from "./src/jobs/nseSyncJob.js";
 
-const PORT  = process.env.PORT || 3001;;
+const PORT = process.env.PORT || 3001;
 
 async function startServer() {
   await connectDB();
-
-  await updateMissingDays();
-
-  startNSESyncJob();
 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
@@ -20,3 +16,6 @@ async function startServer() {
 }
 
 startServer();
+updateMissingDays();
+
+startNSESyncJob();
